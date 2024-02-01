b3a3c375f0415ff1e73c0fdbf92bbda37bc2a1fd
FireFlyRT <43859159+FireFlyRT@users.noreply.github.com>
2024-02-01 23:25:59 +0100

    Decal Spawn

--- Assets/TestScripts/DropWithSplash.cs
+++ Assets/TestScripts/DropWithSplash.cs
@@ -4,11 +4,16 @@ public class DropWithSplash : MonoBehaviour
 {
     [SerializeField]
     private GameObject decal;
+    [SerializeField]
+    private GameObject obj;
 
     private void OnTriggerEnter(Collider other)
     {
-        GameObject dec = Instantiate(decal);
-        dec.transform.position = transform.position;
-        Destroy(this.gameObject);
+        if (obj.GetComponent<Collider>() != other) 
+        {
+            GameObject dec = Instantiate(decal);
+            dec.transform.position = transform.position;
+            Destroy(this.gameObject);
+        }
     }
 }
